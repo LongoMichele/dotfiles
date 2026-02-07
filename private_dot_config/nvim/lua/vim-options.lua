57ffb5b6df0aa3713ac1861b41244ebbbddbed1c
Michele Longo <longomichele2015@gmail.com>
2026-02-07 10:46:49 +0100

    Changed move controls

--- private_dot_config/nvim/lua/vim-options.lua
+++ private_dot_config/nvim/lua/vim-options.lua
@@ -23,9 +23,9 @@ vim.keymap.set("v", "<leader>\"", "c\"\"<Esc>P", tabOptions)
 
 vim.opt.clipboard = 'unnamedplus'
 
-vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-k>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-i>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 vim.opt.termguicolors = true
